06752122d019d942805fdcd83c392a6e5e3b2cf4
carlos eduardo damian perez <dapecarlos@gmail.com>
2023-07-03 17:44:50 -0600

    feat(app): new exersice

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -73,14 +73,27 @@ function useUser() {
   return context
 }
 
+const updateUser = async (dispatch, user, updates) => {
+  dispatch({type: 'start update', updates: updates})
+  try {
+    const updatedUser = await userClient.updateUser(user, updates)
+    dispatch({type: 'finish update', updatedUser})
+    return updatedUser
+  } catch (error) {
+    dispatch({type: 'fail update', error});
+    return Promise.reject(error)
+  }
+}
+
 // 🐨 add a function here called `updateUser`
 // Then go down to the `handleSubmit` from `UserSettings` and put that logic in
 // this function. It should accept: dispatch, user, and updates
 
-// export {UserProvider, useUser}
-
 // src/screens/user-profile.js
+
+// export {UserProvider, useUser,updateUser}
 // import {UserProvider, useUser} from './context/user-context'
+
 function UserSettings() {
   const [{user, status, error}, userDispatch] = useUser()
 
@@ -98,11 +111,7 @@ function UserSettings() {
   function handleSubmit(event) {
     event.preventDefault()
     // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
+    updateUser(userDispatch, user, formState).then((test)=>console.log('tyetsts',test));
   }
 
   return (
--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,36 +4,52 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function Toggle() {
+function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
-
   // 🐨 replace this with a call to React.Children.map and map each child in
   // props.children to a clone of that child with the props they need using
   // React.cloneElement.
   // 💰 React.Children.map(props.children, child => {/* return child clone here */})
   // 📜 https://reactjs.org/docs/react-api.html#reactchildren
   // 📜 https://reactjs.org/docs/react-api.html#cloneelement
-  return <Switch on={on} onClick={toggle} />
-}
 
+  const tests = React.Children.map(children, (child, index) => {
+    if (typeof child.type === 'string') {
+      return child
+    }
+    return React.cloneElement(child, {
+      on,
+      toggle,
+    })
+  })
+  return tests
+}
 // 🐨 Flesh out each of these components
 
 // Accepts `on` and `children` props and returns `children` if `on` is true
-const ToggleOn = () => null
+const ToggleOn = ({on, children}) => (on ? children : undefined)
 
 // Accepts `on` and `children` props and returns `children` if `on` is false
-const ToggleOff = () => null
+const ToggleOff = ({on, children}) => (!on ? children : undefined)
 
-// Accepts `on` and `toggle` props and returns the <Switch /> with those props.
-const ToggleButton = () => null
+// Accepts `on` and `toggle` props and returns the  with those props.
+const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle} />
 
 function App() {
   return (
+    // <div>
+    //   <Toggle>
+    //     <ToggleOn>The button is on</ToggleOn>
+    //     <ToggleOff>The button is off</ToggleOff>
+    //     <ToggleButton />
+    //   </Toggle>
+    // </div>
     <div>
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <span>Hello</span>
         <ToggleButton />
       </Toggle>
     </div>
